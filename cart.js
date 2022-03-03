90d7924b359d4d70b16b6fda9ad0ab56e38c3d15
JHOUJHOU <a01450887@gmail.com>
2022-03-03 23:49:24 +0800

    刪除購物車品項功能

--- cart.js
+++ cart.js
@@ -10,6 +10,8 @@ const app = createApp({
     return {
       cartData: {},
       products: [],
+      productId: '',
+      isLoading: ''
     };
   },
   methods: {
@@ -24,10 +26,88 @@ const app = createApp({
           console.log(err);
         })
     },
+    openProductModal(id) {
+      this.productId = id;
+      this.$refs.productModal.openModal();
+    },
+    getCart() {
+      const apiUrl = `${url}/api/${api_path}/cart`;
+      axios.get(apiUrl)
+        .then((res) => {
+          console.log(res);
+          this.cartData = res.data.data;
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    },
+    addToCart(id , qty = 1) {
+      const data = {
+        product_id : id,
+        qty,
+      };
+      const apiUrl = `${url}/api/${api_path}/cart`;
+      this.isLoading = id;
+      axios.post(apiUrl , { data })
+        .then((res) => {
+          this.getCart();
+          this.isLoading = '';
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    },
+    removeCart(id) {
+      const apiUrl = `${url}/api/${api_path}/cart/${id}`;
+      this.isLoading = id;
+      axios.delete(apiUrl)
+        .then((res) => {
+          this.getCart();
+          this.isLoading = '';
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    }
   },
   mounted() {
     this.getProducts();
+    this.getCart();
   },
+});
+
+app.component('product-modal', {
+  props: ['id'],
+  template : '#userProductModal',
+  data() {
+    return {
+      modal: '',
+      product: {}
+    }
+  },
+  watch: {
+    id() {
+      this.getProduct();
+    }
+  },
+  methods: {
+    openModal() {
+      this.modal.show();
+    },
+    getProduct() {
+      const apiUrl = `${url}/api/${api_path}/product/${this.id}`;
+      axios.get(apiUrl)
+        .then((res) => {
+          this.product = res.data.product;
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    }
+  },
+  mounted() {
+    this.modal = new bootstrap.Modal(this.$refs.modal);
+  }
 });
 
 app.mount('#app');
